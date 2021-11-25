05882c736d329cb5e384699934e98bfbf9161a9b
Christopher MANEU <christopher@maneu.net>
2021-11-25 08:46:23 +0000

    Create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mslearn_time_series_insights_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
